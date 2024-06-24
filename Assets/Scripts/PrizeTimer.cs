2a793e227525b793d39c14d52339ec632424db81
Youggood <ymnik50s@gmail.com>
2024-06-24 19:09:05 +0300

    Добавлены таймеры(логические, а не визуальные), управление этими таймерами, а также enum, отвечающий за редкость банана

---
+++ Assets/Scripts/PrizeTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PrizeTimer 
+    {
+        public bool IsEnd {  get; private set; } = true;
+
+        private float _remainingTime;
+
+        public void SetTime(float remainingTime)
+        {
+            _remainingTime = remainingTime;
+            IsEnd = false;
+        }
+
+
+        public void OnUpdate()
+        {
+            if (!IsEnd)
+            {
+                _remainingTime -= Time.deltaTime;
+
+                if (_remainingTime <= 0)
+                {
+                    IsEnd = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TimersController : MonoBehaviour
+    {
+        private Dictionary<RarityType, PrizeTimer> _timers;
+
+
+        private void Awake()
+        {
+            _timers = new Dictionary<RarityType, PrizeTimer> ();
+            InitTimers();
+        }
+
+        private void Start()
+        {
+            StartTimers();
+        }
+
+
+        private void Update()
+        {
+            foreach (var timer in _timers)
+            {
+                if (timer.Value.IsEnd)
+                {
+                    continue;
+                }
+
+                timer.Value.OnUpdate();
+
+                if (timer.Value.IsEnd)
+                {
+                    
+                    //ToDo ChangingButtons
+                }
+
+            }
+        }
+
+
+        private void StartTimers()
+        {
+            foreach (var _timer in _timers)
+            {
+                _timer.Value.SetTime(UnityEngine.Random.value * 10f);
+            }
+        }
+
+        private void InitTimers()
+        {
+            Array rarityTypes = Enum.GetValues (typeof(RarityType));
+
+            for (int i = 0; i < rarityTypes.Length; i++)
+            {
+                RarityType type = (RarityType) rarityTypes.GetValue(i);
+                PrizeTimer prizeTimer = new PrizeTimer();
+
+                _timers.TryAdd( type, prizeTimer );
+
+            }
+        }
+
+    }
+}
